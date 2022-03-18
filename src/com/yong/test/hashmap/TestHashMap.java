2dfd42498e39ee907d566b3736141ef46c658afe
yongfeng <547563851@qq.com>
2022-03-18 16:34:42 +0800

    jdk源码阅读java项目

---
+++ src/com/yong/test/hashmap/TestHashMap.java
@@ -0,0 +1,19 @@
+package com.yong.test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yongfeng
+ */
+public class TestHashMap
+{
+    public static void main(String[] args)
+    {
+        Map map = new HashMap();
+        map.put("1", "yongfeng");
+        map.put("2", "zhangsan");
+        map.put("3", "lisi");
+    }
+
+}
